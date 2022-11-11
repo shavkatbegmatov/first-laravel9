88a7357d61e7618979c22fe41c9e2e755984510f
SHAVKAT BEGMATOV <fstudioadm@gmail.com>
2022-11-11 11:42:52 +0500

    translate

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -30,7 +30,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<h1 class="text-center my-header">WHO WE ARE</h1>
+				<h1 class="text-center my-header" style="text-transform: uppercase;">{{ __('messages.header-who-we-are') }}</h1>
 			</div>
 		</div>
 		<div class="row">
@@ -44,9 +44,9 @@
 			</div>
 
 			<div class="col-lg-6 col-12">
-				<p class="my-p">Mahorat & Management was founded by a group of expert-initiators on professional education as an independent research and training center to support HRD - Human Resource Development.</p>
-				<p class="my-p">We are non-governmental educational organization that has been operating since 2015 in the field of adults training, ensuring the quality of education and introducing international standards to local educational process, including the introduction of advanced forms of training and retraining using information technologies, improving curriculum and materials based on the widespread use of modern teaching methods.</p>
-				<p class="my-p">Mahorat & Management actively cooperates with governmental and non-governmental organizations, and implements short and long term educational projects.</p>
+				<p class="my-p">{{ __('messages.section-who-we-are-1') }}</p>
+				<p class="my-p">{{ __('messages.section-who-we-are-2') }}</p>
+				<p class="my-p">{{ __('messages.section-who-we-are-3') }}</p>
 			</div>
 		</div>
 	</div>
@@ -57,7 +57,7 @@
 		<div class="row">
 
 			<div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 col-xxl-8" style="background-color: rgba(0, 0, 0, 0.5);padding:5rem;">
-				<p class="my-p" style="font-size: 1.8rem; font-weight: 900; color:aliceblue">Our mission is to unite and build the capacity of experts in order to improve the mechanisms and systems of vocational education, based on international quality indicators and the requirements of the international labor market.</p>
+				<p class="my-p" style="font-size: 1.8rem; font-weight: 900; color:aliceblue">{{ __('messages.section-about-mission') }}</p>
 			</div>
 
 		</div>
@@ -68,12 +68,12 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 col-12">
-				<h4 >We aim towards realizing the following key components:</h4>
+				<h4 >{{ __('messages.aim-towards') }}</h4>
 				<div class="color-box">
 					<div class="shadow">
 						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
 						<div class="note-box">
-							<p class="my-p"><strong class="upper">educational</strong> - to develop high-quality education, to assist the professional development of managers and teachers, professional development and retraining of highly qualified specialists based on the principle of LLL;</p>
+							<p class="my-p"><strong class="upper">{{ __('messages.aim-educational-1') }}</strong> - {{ __('messages.aim-educational-2') }};</p>
 						</div>
 					</div>
 				</div>
@@ -81,7 +81,7 @@
 					<div class="shadow">
 						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
 						<div class="note-box">
-							<p class="my-p"><strong class="upper">cultural</strong> - to be a cultural educational center, contributing to a change in the social environment, creating a certain spiritual atmosphere, teaching intelligent and constructive problem solving;</p>
+							<p class="my-p"><strong class="upper">{{ __('messages.aim-cultural-1') }}</strong> - {{ __('messages.aim-cultural-2') }};</p>
 						</div>
 					</div>
 				</div>
@@ -89,7 +89,7 @@
 					<div class="shadow">
 						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
 						<div class="note-box">
-							<p class="my-p"><strong class="upper">innovative</strong> - to provide quality education in accordance with the UNESCO Concept "Education - 2030" and SDG 4 "Quality Education" with the use of new methods and technologies in retraining and advanced training of specialists, developing innovative thinking for people over 18 years old;</p>
+							<p class="my-p"><strong class="upper">{{ __('messages.aim-innovative-1') }}</strong> - {{ __('messages.aim-innovative-2') }};</p>
 						</div>
 					</div>
 				</div>
@@ -97,7 +97,7 @@
 					<div class="shadow">
 						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
 						<div class="note-box">
-							<p class="my-p"><strong class="upper">constructive</strong> - we build a constructive partnership with the local governmental and nongovernmental organizations and individuals according to their interest in a partnership in HRD.</p>
+							<p class="my-p"><strong class="upper">{{ __('messages.aim-constructive-1') }}</strong> - {{ __('messages.aim-constructive-2') }}.</p>
 						</div>
 					</div>
 				</div>
@@ -115,55 +115,54 @@
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<h1 class="text-center my-header">HOW WE WORK</h1>
+				<h1 class="text-center my-header">{{ __('messages.how-we-work') }}</h1>
 			</div>
 		</div>
 		<div class="row mb-2">
-			<div class="col-md-6">
-				<div class="card flex-md-row mb-4 box-shadow h-md-250">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6">
+				<div class="card flex-md-row mb-4 box-shadow h-md-350">
 					<div class="card-body d-flex flex-column align-items-start">
-						<strong class="d-inline-block mb-2 text-primary">World</strong>
-						<h5 class="mb-0"><a class="text-dark" href="#">Participation in tenders</a></h5>
-						<p class="card-text mb-auto">Participation in tenders for social order to improve the systems of vocational education and training at all levels (ISCED and ISCO):
-							Management of the educational process
-							Recognition of prior learning (RPL)
-							Systematization, automation and launching of educational processes and systems.</p>
-						<a href="#">Continue reading</a>
+						{{-- <strong class="d-inline-block mb-2 text-primary">World</strong> --}}
+						<h5 class="mb-0"><a class="text-dark" href="#">{{ __('messages.participation-in-tenders-1') }}</a></h5>
+						<p class="card-text mb-auto">{{ __('messages.participation-in-tenders-2') }}
+							<ul class="check-list">
+								<li>{{ __('messages.participation-in-tenders-3') }}</li>
+								<li>{{ __('messages.participation-in-tenders-4') }}</li>
+								<li>{{ __('messages.participation-in-tenders-5') }}</li>
+							</ul>
+						</p>
+						{{-- <a href="#">Continue reading</a> --}}
 					</div>
-					<img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
 				</div>
 			</div>
-			<div class="col-md-6">
-				<div class="card flex-md-row mb-4 box-shadow h-md-250">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6">
+				<div class="card flex-md-row mb-4 box-shadow h-md-350">
 					<div class="card-body d-flex flex-column align-items-start">
-						<strong class="d-inline-block mb-2 text-primary">World</strong>
-						<h5 class="mb-0"><a class="text-dark" href="#">Outsourcing services</a></h5>
-						<p class="card-text mb-auto">Outsourcing services in certain areas of vocational education and training (VET) and professional development and retraining (CPD-Continuing professional development). RPL - Recognition of Prior Learning.</p>
-						<a href="#">Continue reading</a>
+						{{-- <strong class="d-inline-block mb-2 text-primary">World</strong> --}}
+						<h5 class="mb-0"><a class="text-dark" href="#">{{ __('messages.outsourcing-services-1') }}</a></h5>
+						<p class="card-text mb-auto">{{ __('messages.outsourcing-services-2') }}</p>
+						{{-- <a href="#">Continue reading</a> --}}
 					</div>
-					<img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
 				</div>
 			</div>
-			<div class="col-md-6">
-				<div class="card flex-md-row mb-4 box-shadow h-md-250">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6">
+				<div class="card flex-md-row mb-4 box-shadow h-md-350">
 					<div class="card-body d-flex flex-column align-items-start">
-						<strong class="d-inline-block mb-2 text-primary">World</strong>
-						<h5 class="mb-0"><a class="text-dark" href="#">Exchange programs</a></h5>
-						<p class="card-text mb-auto">Organization of exchange programs between educators, researchers, and supervisors.</p>
-						<a href="#">Continue reading</a>
+						{{-- <strong class="d-inline-block mb-2 text-primary">World</strong> --}}
+						<h5 class="mb-0"><a class="text-dark" href="#">{{ __('messages.exchange-programs-1') }}</a></h5>
+						<p class="card-text mb-auto">{{ __('messages.exchange-programs-2') }}</p>
+						{{-- <a href="#">Continue reading</a> --}}
 					</div>
-					<img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
 				</div>
 			</div>
-			<div class="col-md-6">
-				<div class="card flex-md-row mb-4 box-shadow h-md-250">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6">
+				<div class="card flex-md-row mb-4 box-shadow h-md-350">
 					<div class="card-body d-flex flex-column align-items-start">
-						<strong class="d-inline-block mb-2 text-primary">World</strong>
-						<h5 class="mb-0"><a class="text-dark" href="#">Support for youth initiatives</a></h5>
-						<p class="card-text mb-auto">Support for youth initiatives aimed at the welfare of the population, by building the capacity of human capital.</p>
-						<a href="#">Continue reading</a>
+						{{-- <strong class="d-inline-block mb-2 text-primary">World</strong> --}}
+						<h5 class="mb-0"><a class="text-dark" href="#">{{ __('messages.support-for-youth-initiatives-1') }}</a></h5>
+						<p class="card-text mb-auto">{{ __('messages.support-for-youth-initiatives-2') }}</p>
+						{{-- <a href="#">Continue reading</a> --}}
 					</div>
-					<img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
 				</div>
 			</div>
 		</div>
