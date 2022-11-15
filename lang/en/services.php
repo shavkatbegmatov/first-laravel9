340ccf3fd7525b32c4486c1dfff77a135b3a0f3d
SHAVKAT BEGMATOV <fstudioadm@gmail.com>
2022-11-15 18:32:36 +0500

    service cards

---
+++ lang/en/services.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'title' => 'This is English Language Title.',
+	'header-services' => 'Services',
+	'sub-header-services' => '',
+	'our-services' => 'Our Services',
+
+
+];
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -10,14 +10,13 @@
  
 @section('content')
 
-<header class="site-header site-about-header">
+<header class="site-header site-services-header">
 	<div class="container">
 		<div class="row">
 
 			<div class="col-lg-10 col-12 mx-auto">
-				<h1 class="text-white my-font">{{ __('messages.header-who-we-are') }}</h1>
-
-				<strong class="text-white my-font">{{ __('messages.sub-header-we-are') }}</strong>
+				<h1 class="text-white my-font">{{ __('services.header-services') }}</h1>
+				<strong class="text-white my-font">{{ __('services.sub-header-services') }}</strong>
 			</div>
 
 		</div>
@@ -26,118 +25,33 @@
 	<div class="overlay"></div>
 </header>
 
+
+
 <section class="about section-padding bg-white">
 	<div class="container">
-		<div class="row">
+		<div class="row my-header-row">
 			<div class="col">
-				<h1 class="text-center my-header" style="text-transform: uppercase;">{{ __('messages.header-who-we-are') }}</h1>
+				<h1 class="text-center my-header">{{ __('services.our-services') }}</h1>
 			</div>
 		</div>
-		<div class="row">
-			<div class="position-relative col-lg-6 col-12 mb-2" style="background-image: url(./assets/images/human-resource-development.jpg); background-size: cover;min-height: 30rem">
-				{{-- <img width = 100% src="./assets/images/human-resource-development.jpg" /> --}}
-				{{-- <h4 class="mb-3">Human Resource Development</h4> --}}
-				<div class="position-absolute bottom-0 end-0" style="margin: 1rem">
-					{{-- <a href="news.html" class="custom-btn btn btn-dark mt-3">Check out News</a> --}}
-					<a href="contact.html" class="custom-btn btn btn-success danger mt-3 ms-3">Say Hi</a>
-				</div>
-			</div>
 
-			<div class="col-lg-6 col-12">
-				<p class="my-p">{{ __('messages.section-who-we-are-1') }}</p>
-				<p class="my-p">{{ __('messages.section-who-we-are-2') }}</p>
-				<p class="my-p">{{ __('messages.section-who-we-are-3') }}</p>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="about mission section-padding">
-	<div class="container">
-		<div class="row">
-
-			<div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 col-xxl-8" style="background-color: rgba(0, 0, 0, 0.5);padding:5rem;">
-				<p class="my-p" style="font-size: 1.8rem; font-weight: 900; color:aliceblue">{{ __('messages.section-about-mission') }}</p>
-			</div>
-
-		</div>
-	</div>
-</section>
-
-<section class="about section-padding bg-white">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8 col-12">
-				<h4 class="my-font">{{ __('messages.aim-towards') }}</h4>
-				<div class="color-box">
-					<div class="shadow">
-						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
-						<div class="note-box">
-							<p class="my-p"><strong class="upper my-font">{{ __('messages.aim-educational-1') }}</strong> - {{ __('messages.aim-educational-2') }};</p>
-						</div>
-					</div>
-				</div>
-				<div class="color-box">
-					<div class="shadow">
-						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
-						<div class="note-box">
-							<p class="my-p"><strong class="upper my-font">{{ __('messages.aim-cultural-1') }}</strong> - {{ __('messages.aim-cultural-2') }};</p>
-						</div>
-					</div>
-				</div>
-				<div class="color-box">
-					<div class="shadow">
-						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
-						<div class="note-box">
-							<p class="my-p"><strong class="upper my-font">{{ __('messages.aim-innovative-1') }}</strong> - {{ __('messages.aim-innovative-2') }};</p>
-						</div>
-					</div>
-				</div>
-				<div class="color-box">
-					<div class="shadow">
-						<div class="info-tab note-icon" title="Important Notes"><i></i></div>
-						<div class="note-box">
-							<p class="my-p"><strong class="upper my-font">{{ __('messages.aim-constructive-1') }}</strong> - {{ __('messages.aim-constructive-2') }}.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="position-relative col-lg-4 col-12 mb-2" style="background-image: url(./assets/images/about-key-components-2.jpg); background-size: cover;min-height: 30rem; background-position: center;">
-				<div class="position-absolute bottom-0 end-0" style="margin: 1rem">
-					<a href="contact.html" class="custom-btn btn btn-success danger mt-3 ms-3">Say Hi</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="about section-padding bg-white">
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<h1 class="text-center my-header">{{ __('messages.how-we-work') }}</h1>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-12">
-				<p class="my-p" style="padding-top: 3rem; font-size: 1.5rem">{{ __('messages.our-strategy') }}</p>
-			</div>
-		</div>
 		<div class="row mb-2">
 			<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6">
-				<div class="card flex-md-row mb-4 box-shadow h-md-350">
-					<div class="card-body d-flex flex-column align-items-start">
-						{{-- <strong class="d-inline-block mb-2 text-primary">World</strong> --}}
-						<h5 class="mb-0"><a class="text-dark my-font" href="">{{ __('messages.participation-in-tenders-1') }}</a></h5>
-						<p style="margin-bottom: 0 !important;" class="card-text mb-auto">{{ __('messages.participation-in-tenders-2') }}</p>
-						<ul class="check-list">
-							<li class="my-font">{{ __('messages.participation-in-tenders-3') }}</li>
-							<li class="my-list">{{ __('messages.participation-in-tenders-4') }}</li>
-							<li class="my-list">{{ __('messages.participation-in-tenders-5') }}</li>
-						</ul>
-						{{-- <a href="#">Continue reading</a> --}}
+				{{-- <div class="card flex-md-row mb-4 box-shadow h-md-350"> --}}
+					<div class="my-card-parent">
+						<div class="my-card-left">
+							1
+						</div>
+						<div class="my-card-right">
+							<div class="my-cart-header">
+								2
+							</div>
+							<div class="my-cart-text">
+								3
+							</div>
+						</div>
 					</div>
-				</div>
+				{{-- </div> --}}
 			</div>
 			<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6">
 				<div class="card flex-md-row mb-4 box-shadow h-md-350">
