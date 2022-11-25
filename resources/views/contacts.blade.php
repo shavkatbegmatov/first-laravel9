07a909fa41ae9de272b8e54f83ebb448bfd39b79
shavkatbegmatov <fstudioadm@gmail.com>
2022-11-25 08:30:39 +0500

    myhome

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -32,131 +32,113 @@
 		<div class="row my-header-row">
 			<div class="col">
 				<h2 class="text-center my-header">{{ __('contacts.our-experts') }}</h2>
-				{{-- <p class="my-p">{{ __('experts.our-experts-text') }}</p> --}}
+				{{-- <p class="my-p">{{ __('contacts.our-experts-text') }}</p> --}}
 			</div>
 		</div>
 	</div>
 </section>
 
-<link href="{{asset('assets/team/sch-style1.css')}}" rel="stylesheet" />
-<div class="tab-pane fade active show" id="nav-demo-1" role="tabpanel" aria-labelledby="nav-demo-1-tab">
-	<div class="sch-style-light">
-		<div class="container">
-			{{-- <h6 class="sch-title"><i class="fa fa-asterisk"></i> Background Light</h6> --}}
-			<div class="row">
 
-				<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-
-				</div>
+<link href="{{asset('assets/contact/css/style.css')}}" rel="stylesheet" />
 
 
-			</div>
-		</div>
-	</div>
-</div>
+<section class="ftco-section">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-12">
+				<div class="wrapper">
 
-
-
-	<section class="ftco-section">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-12">
-					<div class="wrapper">
-						<div class="row no-gutters mb-5">
-							<div class="col-md-7">
-								<div class="contact-wrap w-100 p-md-5 p-4">
-									<h3 class="mb-4">Contact Us</h3>
-									<div id="form-message-warning" class="mb-4"></div> 
-				      		<div id="form-message-success" class="mb-4">
-				            Your message was sent, thank you!
-				      		</div>
-									<form method="POST" id="contactForm" name="contactForm" class="contactForm">
-										<div class="row">
-											<div class="col-md-6">
-												<div class="form-group">
-													<label class="label" for="name">Full Name</label>
-													<input type="text" class="form-control" name="name" id="name" placeholder="Name">
-												</div>
-											</div>
-											<div class="col-md-6"> 
-												<div class="form-group">
-													<label class="label" for="email">Email Address</label>
-													<input type="email" class="form-control" name="email" id="email" placeholder="Email">
-												</div>
-											</div>
-											<div class="col-md-12">
-												<div class="form-group">
-													<label class="label" for="subject">Subject</label>
-													<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
-												</div>
-											</div>
-											<div class="col-md-12">
-												<div class="form-group">
-													<label class="label" for="#">Message</label>
-													<textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message"></textarea>
-												</div>
-											</div>
-											<div class="col-md-12">
-												<div class="form-group">
-													<input type="submit" value="Send Message" class="btn btn-primary">
-													<div class="submitting"></div>
-												</div>
-											</div>
-										</div>
-									</form>
+					<div class="row" style="margin-bottom: 7rem;">
+						<div class="col-md-4">
+							<div class="dbox w-100 text-center">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="fa fa-map-marker"></span>
+								</div>
+								<div class="text">
+									<p><span>Address:</span> Shota Rustavili Street 53B <br /> Tashkent, Uzbekistan 100003</p>
 								</div>
 							</div>
-							<div class="col-md-5 d-flex align-items-stretch">
-								<div id="map"></div>
+						</div>
+						<div class="col-md-4">
+							<div class="dbox w-100 text-center">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="fa fa-phone"></span>
+								</div>
+								<div class="text">
+									<p><span>Phone:</span> <a href="tel://998951950055">+998 95 195-00-55</a></p>
+								</div>
 							</div>
 						</div>
-						<div class="row">
-							<div class="col-md-3">
-								<div class="dbox w-100 text-center">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-map-marker"></span>
-			        		</div>
-			        		<div class="text">
-				            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
-				          </div>
-			          </div>
-							</div>
-							<div class="col-md-3">
-								<div class="dbox w-100 text-center">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-phone"></span>
-			        		</div>
-			        		<div class="text">
-				            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
-				          </div>
-			          </div>
-							</div>
-							<div class="col-md-3">
-								<div class="dbox w-100 text-center">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-paper-plane"></span>
-			        		</div>
-			        		<div class="text">
-				            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-				          </div>
-			          </div>
-							</div>
-							<div class="col-md-3">
-								<div class="dbox w-100 text-center">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-globe"></span>
-			        		</div>
-			        		<div class="text">
-				            <p><span>Website</span> <a href="#">yoursite.com</a></p>
-				          </div>
-			          </div>
+						<div class="col-md-4">
+							<div class="dbox w-100 text-center">
+								<div class="icon d-flex align-items-center justify-content-center">
+									<span class="fa fa-paper-plane"></span>
+								</div>
+								<div class="text">
+									<p><span>Email:</span> <a href="mailto:info@mahorat.org">info@mahorat.org</a></p>
+								</div>
 							</div>
 						</div>
 					</div>
+
+					<div class="row no-gutters mb-5">
+						<div class="col-md-7">
+							<div class="contact-wrap w-100 p-md-5 p-4">
+								<h3 class="mb-4">Contact Us</h3>
+								<div id="form-message-warning" class="mb-4"></div>
+								<div id="form-message-success" class="mb-4">
+									Your message was sent, thank you!
+								</div>
+								<form method="POST" id="contactForm" name="contactForm" class="contactForm">
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="label" for="name">Full Name</label>
+												<input type="text" class="form-control" name="name" id="name" placeholder="Name">
+											</div>
+										</div>
+										<div class="col-md-6"> 
+											<div class="form-group">
+												<label class="label" for="email">Email Address</label>
+												<input type="email" class="form-control" name="email" id="email" placeholder="Email">
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="form-group">
+												<label class="label" for="subject">Subject</label>
+												<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="form-group">
+												<label class="label" for="#">Message</label>
+												<textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message"></textarea>
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="form-group">
+												<input type="submit" value="Send Message" class="btn btn-primary">
+												<div class="submitting"></div>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="col-md-5 d-flex align-items-stretch">
+							<div id="map">
+								
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5996.346567646579!2d69.251601!3d41.283333!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x672cf1e7743ccd33!2zNDHCsDE3JzAwLjAiTiA2OcKwMTUnMDUuOCJF!5e0!3m2!1sen!2s!4v1658926045086!5m2!1sen!2s" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 <script src="{{asset('assets/contact/js/jquery.min.js')}}"></script>
 <script src="{{asset('assets/contact/js/popper.js')}}js/popper.js"></script>
