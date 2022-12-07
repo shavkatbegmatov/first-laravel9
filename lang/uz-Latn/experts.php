43ef2eae75d6bcf8b07f67fd7cdbe7b03a3b330b
SHAVKAT BEGMATOV <fstudioadm@gmail.com>
2022-12-07 12:45:29 +0500

    эксперт таржима

--- lang/uz-Latn/experts.php
+++ lang/uz-Latn/experts.php
@@ -8,8 +8,8 @@ return [
 	
 	'khudaynazar-kurbanov-1' => 'Xudaynazar',
 	'khudaynazar-kurbanov-2' => 'Kurbanov',
-	'khudaynazar-kurbanov-work-1' => 'Founder/CEO',
-	'khudaynazar-kurbanov-work-2' => 'Mahorat & Management',
+	'khudaynazar-kurbanov-work-1' => 'Mahorat & Management',
+	'khudaynazar-kurbanov-work-2' => 'asoschisi / bosh direktori',
 	'khudaynazar-kurbanov-more' => 'Ilmiy xodim (kasb-hunar ta’limi sohasida). NQF, ISCED (UNESCO), Ta‘lim sifatini ta’minlash sohalarida xalqaro tajribaga ega mutaxassis. Kontent ishlab chiqaruvchisi. Loyihalar bo’yicha menejer. Mahorat & Management tashkiloti hammuassisi. Xalqaro va milliy loyihalarda 18 yillik konsalting tajribasiga ega.',
 
 	'feruza-rashidova-1' => 'Dr. Feruza',
@@ -20,51 +20,51 @@ return [
 
 	'shuhratxoja-amanov-1' => 'Shuhratxo‘ja',
 	'shuhratxoja-amanov-2' => 'Amanov',
-	'shuhratxoja-amanov-work-1' => 'Skills & Employment expert',
-	'shuhratxoja-amanov-work-2' => 'Education',
+	'shuhratxoja-amanov-work-1' => 'Malakalar va bandlik',
+	'shuhratxoja-amanov-work-2' => 'bo‘yicha mutaxassis',
 	'shuhratxoja-amanov-more' => 'MBA darajasiga ega. Biznesni rivojlantirish, ta’lim, malaka va inson kapitalini rivojlantirish sohasida xalqaro tashkilotlarda 20 yildan ortiq tajribaga ega. Armaniston, Gruziya, Qozogʻiston, Tojikiston, Turkmaniston va Oʻzbekistonda donorlar va xalqaro kredit agentliklari tomonidan moliyalashtiriladigan loyihalarni boshqarish va maslahat berish boʻyicha koʻp yillik tajribaga ega.
 ',
 
 	'farhod-furkatov-1' => 'Farhod',
 	'farhod-furkatov-2' => 'Furkatov',
-	'farhod-furkatov-work-1' => 'Data Scientist',
-	'farhod-furkatov-work-2' => 'CEO - Goodly Wellness',
+	'farhod-furkatov-work-1' => 'Maʼlumotlar boʻyicha mutaxassis',
+	'farhod-furkatov-work-2' => 'Goodly Wellness bosh direktori',
 	'farhod-furkatov-more' => 'Mahorat & Management tashkilotining milliy eksperti. Raqamli mahsulotlarni yaratish va joriy etishda 10 yildan ortiq tajribaga ega. Uning texnologiya olamiga sayohati Kaliforniya, Silikon vodiysidagi ma’lumotlar dunyosi bilan tanishishdan boshlandi. U biznes va odamlar uchun sifatli texnologik yechimlarni taqdim etish ishtiyoqiga ega texnologik tadbirkor.',
 
 	'sultanmurad-hamroev-1' => 'Sultanmurad',
 	'sultanmurad-hamroev-2' => 'Hamroev',
-	'sultanmurad-hamroev-work-1' => 'Business Analyst',
-	'sultanmurad-hamroev-work-2' => 'Project Coordinator - TVET',
+	'sultanmurad-hamroev-work-1' => 'Biznes tahlilchisi',
+	'sultanmurad-hamroev-work-2' => 'TVET loyiha koordinatori',
 	'sultanmurad-hamroev-more' => 'Kasb-hunar ta’limi sifati bo‘yicha mutaxassis. “Mahorat & Management” milliy eksperti. Yosh tahlilchilarni rivojlantirish bo‘yicha loyiha menejeri (ADP).',
 
 	'alisher-akmaljonov-1' => 'Alisher',
 	'alisher-akmaljonov-2' => 'Akmaljonov',
-	'alisher-akmaljonov-work-1' => 'Entrepreneur/Founder of',
-	'alisher-akmaljonov-work-2' => '“Event Entertainment” LLC',
+	'alisher-akmaljonov-work-1' => '“Event Entertainment” MChJ asoschisi',
+	'alisher-akmaljonov-work-2' => 'Tadbirkor',
 	'alisher-akmaljonov-more' => 'Tadbirkor. Event Entertainment MChJ asoschisi va direktori. Markaziy Osiyo Yoshlar Liderlik Akademiyasi (CAYLA) aʼzosi.',
 
 	'sadokat-kaziyeva-1' => 'Sadokat',
 	'sadokat-kaziyeva-2' => 'Kaziyeva',
-	'sadokat-kaziyeva-work-1' => 'Product Designer',
-	'sadokat-kaziyeva-work-2' => 'User Experience specialist',
+	'sadokat-kaziyeva-work-1' => 'Mahsulot dizayneri',
+	'sadokat-kaziyeva-work-2' => 'Foydalanuvchi tajribasi bo‘yicha mutaxassis',
 	'sadokat-kaziyeva-more' => 'Sadokat ko‘p yillar davomida kadrlar va rekruting sohasida ishlaganidan so‘ng UI / UX dizayn sohasiga o‘tdi. Odamlarga o‘z muammolarini hal qilishda g‘ayrat bilan yordam beradi va eng yaxshi natijaga erishadi. U dunyoning yetakchi kompaniyalaridan biri — Appleda ishlash tajribasiga ega. U odamlarga o‘z g‘oyalarini oddiy, jozibali va uyg‘un mahsulotlarga aylantirishga yordam berishga intiladi.',
 
 	'sirojiddin-olimov-1' => 'Sirojiddin',
 	'sirojiddin-olimov-2' => 'Olimov',
-	'sirojiddin-olimov-work-1' => 'Member of Central Asian',
-	'sirojiddin-olimov-work-2' => 'Youth Leadership Academy (CAYLA)',
+	'sirojiddin-olimov-work-1' => 'Markaziy Osiyo yoshlar yetakchilik',
+	'sirojiddin-olimov-work-2' => 'akademiyasi (CAYLA) aʼzosi',
 	'sirojiddin-olimov-more' => 'Markaziy Osiyo Yoshlar Liderlik Akademiyasi (CAYLA) aʼzosi. Edinburg universiteti magistratura talabasi.',
 
 	'nodir-ergashev-1' => 'Nodir',
 	'nodir-ergashev-2' => 'Ergashev',
 	'nodir-ergashev-work-1' => 'UzTEA',
-	'nodir-ergashev-work-2' => 'expert',
+	'nodir-ergashev-work-2' => 'eksperti',
 	'nodir-ergashev-more' => 'UzTEA eksperti. UzTEA boshqaruv kengashi a’zosi. Kontent ishlab chiquvchi va murabbiy.',
 
 	'sabrina-bakhtiyorova-1' => 'Sabrina',
 	'sabrina-bakhtiyorova-2' => 'Bakhtiyorova',
-	'sabrina-bakhtiyorova-work-1' => 'Project development',
-	'sabrina-bakhtiyorova-work-2' => 'coordinator',
+	'sabrina-bakhtiyorova-work-1' => 'Loyihani ishlab',
+	'sabrina-bakhtiyorova-work-2' => 'chiqish koordinatori',
 	'sabrina-bakhtiyorova-more' => 'Yosh analytics rivojlantirish loyihasi faoli. Biznes va moliyaviy Menejment bakalavri (MDIST). Metodist.',
 
 
